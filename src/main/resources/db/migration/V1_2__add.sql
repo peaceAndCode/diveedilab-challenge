b61eb8476ee60da94060e0bbfedc277d77cd1fca
Daniele Ravaglia <rav0731@gmail.com>
2024-04-04 16:16:34 +0200

    data enhancement

--- src/main/resources/db/migration/V1_2__add.sql
+++ src/main/resources/db/migration/V1_2__add.sql
@@ -1,2 +1,34 @@
 INSERT INTO PET (id, name, age, breed) VALUES ('32abc7d6-222f-4ef3-b30a-b2dbeac8bfb1', 'pluto', 1, 'beagle');
 INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('068a22d5-a173-4527-a322-e08f7add0cdd', 'fever','32abc7d6-222f-4ef3-b30a-b2dbeac8bfb1', '2024-04-04T13:26:59.906Z', 'ANALGESIC_DRUGS');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('d726a526-a3a5-4e29-bd62-540a82e9661c', 'pain','32abc7d6-222f-4ef3-b30a-b2dbeac8bfb1', '2024-04-04T13:26:59.906Z', 'ANALGESIC_DRUGS');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('5944c73f-38fc-4da4-83ce-8cd45d319a1c', 'itching','32abc7d6-222f-4ef3-b30a-b2dbeac8bfb1', '2024-04-04T13:26:59.906Z', 'ANTIHISTAMINES');
+
+INSERT INTO PET (id, name, age, breed) VALUES ('8c78905e-d4cf-4f71-91f5-5d38cb8df8b5', 'buddy', 3, 'labrador');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('289f1b9c-96e1-472f-a0d0-2d67fc8f4c5d', 'ear infection','8c78905e-d4cf-4f71-91f5-5d38cb8df8b5', '2024-04-04T13:26:59.906Z', 'ANTI_INFLAMMATORY_DRUGS');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('7f025ab2-3635-4e99-885d-72f4d6a7ed1b', 'digestive issues','8c78905e-d4cf-4f71-91f5-5d38cb8df8b5', '2024-04-04T13:26:59.906Z', 'PROBIOTICS');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('55f17467-0582-4f9a-96f0-1b5be03b6866', 'skin rash','8c78905e-d4cf-4f71-91f5-5d38cb8df8b5', '2024-04-04T13:26:59.906Z', 'ANTIHISTAMINES');
+
+INSERT INTO PET (id, name, age, breed) VALUES ('66a88c5a-7e8a-4e88-94fc-d30c58c6aa9f', 'max', 2, 'poodle');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('08ef5881-1472-4d9d-9ab5-3f10c5a21f77', 'eye infection','66a88c5a-7e8a-4e88-94fc-d30c58c6aa9f', '2024-04-04T13:26:59.906Z', 'ANTI_INFLAMMATORY_DRUGS');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('b059dcdb-58de-4d4b-aeda-4b15af16b590', 'joint pain','66a88c5a-7e8a-4e88-94fc-d30c58c6aa9f', '2024-04-04T13:26:59.906Z', 'ANALGESIC_DRUGS');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('76010eb5-94a9-4e9f-a3e5-611d98b36c69', 'allergy','66a88c5a-7e8a-4e88-94fc-d30c58c6aa9f', '2024-04-04T13:26:59.906Z', 'ANTIHISTAMINES');
+
+INSERT INTO PET (id, name, age, breed) VALUES ('6b02b7bc-30a5-45f2-8b0f-04dc75b0f5b5', 'luna', 5, 'siamese');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('f8d3e527-930f-4e77-ba58-13bb1c1c674d', 'urinary tract infection','6b02b7bc-30a5-45f2-8b0f-04dc75b0f5b5', '2024-04-04T13:26:59.906Z', 'ANTI_INFLAMMATORY_DRUGS');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('ac25277d-98b6-4f0e-9bcf-f2ddfd686c8f', 'dehydration','6b02b7bc-30a5-45f2-8b0f-04dc75b0f5b5', '2024-04-04T13:26:59.906Z', 'ANALGESIC_DRUGS');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('810b25ac-1d1d-4813-95cf-593a8d358158', 'vomiting','6b02b7bc-30a5-45f2-8b0f-04dc75b0f5b5', '2024-04-04T13:26:59.906Z', 'ANALGESIC_DRUGS');
+
+INSERT INTO PET (id, name, age, breed) VALUES ('c84a5c2d-14eb-4859-910f-21b623d72a36', 'rocky', 4, 'german shepherd');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('cbe62e17-6190-4980-86a5-b1fe92eae96c', 'arthritis','c84a5c2d-14eb-4859-910f-21b623d72a36', '2024-04-04T13:26:59.906Z', 'ANALGESIC_DRUGS');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('89f0f1d3-25a0-4867-8b52-76b57681f0a4', 'skin allergy','c84a5c2d-14eb-4859-910f-21b623d72a36', '2024-04-04T13:26:59.906Z', 'ANTIHISTAMINES');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('2d5bc57d-8b69-43cf-a0d6-dc6cc27d6aa4', 'gastrointestinal issues','c84a5c2d-14eb-4859-910f-21b623d72a36', '2024-04-04T13:26:59.906Z', 'PROBIOTICS');
+
+INSERT INTO PET (id, name, age, breed) VALUES ('4f125b3d-7ad9-4b5f-8ed9-7f29c3b48ab4', 'simba', 2, 'maine coon');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('9a0a672b-bfd9-47f1-a5e1-4b6d2fb4295d', 'fleas infestation','4f125b3d-7ad9-4b5f-8ed9-7f29c3b48ab4', '2024-04-04T13:26:59.906Z', 'ANTIHISTAMINES');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('e3c6a646-dfad-48b4-9255-10788db08d61', 'hairball','4f125b3d-7ad9-4b5f-8ed9-7f29c3b48ab4', '2024-04-04T13:26:59.906Z', 'LAXATIVES');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('2bcbba32-67f6-4f1e-a9a5-18eb6d3f74a3', 'digestive upset','4f125b3d-7ad9-4b5f-8ed9-7f29c3b48ab4', '2024-04-04T13:26:59.906Z', 'PROBIOTICS');
+
+INSERT INTO PET (id, name, age, breed) VALUES ('2c6d0f4a-2f3d-4d3e-86f2-2e6cf2e3df71', 'toby', 6, 'golden retriever');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('6fd50047-0a01-4e96-87d1-fc128f0b6d6a', 'hip dysplasia','2c6d0f4a-2f3d-4d3e-86f2-2e6cf2e3df71', '2024-04-04T13:26:59.906Z', 'ANTI_INFLAMMATORY_DRUGS');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('fcb4223c-9034-4311-b15a-af8d15010063', 'ear mites','2c6d0f4a-2f3d-4d3e-86f2-2e6cf2e3df71', '2024-04-04T13:26:59.906Z', 'ANTIHISTAMINES');
+INSERT INTO DIAGNOSIS (id, name, pet_id, date, treatment) VALUES ('c11cd464-5e7d-4a2b-ae22-4f4df5a0e571', 'obesity','2c6d0f4a-2f3d-4d3e-86f2-2e6cf2e3df71', '2024-04-04T13:26:59.906Z', 'PROBIOTICS');
